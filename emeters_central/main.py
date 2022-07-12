16deec01ce8bdedac32d6f87ea02266fee4e3cea
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2022-07-13 01:17:12 +0900

    replace while True with asyncio.Event

--- emeters_central/main.py
+++ emeters_central/main.py
@@ -1,9 +1,16 @@
+"""
+Receive data on notification until device disconnects
+
+https://github.com/hbldh/bleak/discussions/658
+https://github.com/hbldh/bleak/blob/7e9b00954baa1538a2d9098be336402ecb7534db/examples/disconnect_callback.py
+"""
 import asyncio
 import logging
 from bleak import BleakClient, BleakScanner
 
 SERVICE_UUID = "0000ffe0-0000-1000-8000-00805f9b34fb"
 CHARACTERISTIC_UUID = "0000ffe1-0000-1000-8000-00805f9b34fb"
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -20,44 +27,24 @@ def on_notify(sender: int, data: bytearray):
 async def main(*, service_uuid: str, char_uuid: str):
     logger.info("scanning device...")
     device = await BleakScanner.discover(service_uuids=[service_uuid])
-    assert isinstance(device, list) and len(device) == 1, \
-        f"no device found by this service UUID({service_uuid})"
-    assert isinstance(device[0].address, str)
+    if not isinstance(device, list) or len(device) != 1:
+        logger.error(f"no device found (service {service_uuid})")
+        return
 
+    assert isinstance(device[0].address, str)
     addr = device[0].address
-    async with BleakClient(address_or_ble_device=addr) as client:
+
+    disconnected_event = asyncio.Event() # not thread-safe
+
+    def on_disconnect(client: BleakClient):
+        logger.info("disconnected")
+        disconnected_event.set()
+
+    async with BleakClient(address_or_ble_device=addr, disconnected_callback=on_disconnect) as client:
         logger.info(f"connected to {client}")
         await client.start_notify(char_specifier=char_uuid, callback=on_notify)
-
-        try:
-            while True:
-                await asyncio.sleep(2.0)
-                pass
-        except KeyboardInterrupt:
-            await client.stop_notify(char_specifier=char_uuid)
-            logger.info(f"connection end")
-            return
-
-
-#async def main():
-#    async with BleakScanner(service_uuids=[UUID_SERVICE]) as scanner: # with filter
-#    #async with BleakScanner() as scanner: # w/o filter
-#        await asyncio.sleep(5.0)
-#
-#    for d in scanner.discovered_devices:
-#        print(d.details)
-
-
-#async def main():
-#    """scan using filter"""
-#    device = await BleakScanner.find_device_by_filter(
-#        lambda d, ad: UUID_SERVICE in ad.service_uuids, 10.0
-#    )
-#    if device is None:
-#        print("Not found")
-#        return
-#
-#    print(device)
+        logger.info("sleeping until device disconnects...")
+        await disconnected_event.wait() # sleep until set() is called
 
 
 if __name__ == "__main__":
